c1279e77c42d9faa96382c2a6104ab6a2c3ac4df
Fang <2968697144@qq.com>
2019-04-12 18:12:20 +0800

    简单的动画控制

---
+++ AnimationCtrl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+///<summary>
+///按下Q W E 播放动画 
+///动画名字必须为01 02 03
+///最多添加三个动画
+///</summary>
+[RequireComponent(typeof(Animation))]
+public class AnimationCtrl : MonoBehaviour 
+{
+    private Animation ani;
+    private void Start()
+    {
+        ani = GetComponent<Animation>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            ani.Play("01");
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            ani.Play("02");
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            ani.Play("03");
+        }
+    }
+}
